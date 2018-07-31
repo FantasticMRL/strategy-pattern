1ede6360723fbb6b27e606b9a2f3f7b93c8fcde2
companycomputer <544520418@qq.com>
2018-07-31 10:28:05 +0800

    commnet

--- src/com/mrl/Demo.java
+++ src/com/mrl/Demo.java
@@ -1,5 +1,13 @@
 package com.mrl;
 
+/**
+ *  客户端
+ *  [功能详细描述]
+ * @作者 lwqMR
+ * @version [版本号, 2018年7月31日]
+ * @see [相关类/方法]
+ * @since [产品/模块版本] 
+ */
 public class Demo
 {
     public static void main(String[] args) {
